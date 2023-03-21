c23d3d0e0b381161ede228b18b337c7f7b1da047
GKFitz123 <gkf2103@gmail.com>
2023-03-20 20:43:24 -0400

    work on the coffee and cocktail api

--- js/script.js
+++ js/script.js
@@ -1,53 +1,5 @@
 
-//Rapid API Random cat API
-
-
-
-//RapidAPI Tasty API Coffee/Cocktails;
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "9fa51d6725msha4068349b62f730p12c6d2jsnbe63b8ef2e2d",
-		"X-RapidAPI-Host": "tasty.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-
-
-
-
-
-
-
-// NOt Rapid CATS API
-// const promise = $.ajax(
-//     'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_dyq3XlD3i6ROIfc1C5svJpaWpdLzo5dQyso9rpuUoThjnPfIPgmN5uvA1ylbQ59y'
-	
-// );
-
-// promise.then(
-//   (data) => {
-//    console.log(data);
-//   },
-//   (error) => {
-//    console.log('bad request: ', error);
-//   }
-// );
-
-// const url = "https://api.thecatapi.com/v1/images/search?limit=10";
-
-// const api_key = "live_dyq3XlD3i6ROIfc1C5svJpaWpdLzo5dQyso9rpuUoThjnPfIPgmN5uvA1ylbQ59y";
-
-
-
-//  Not rapid API CATS
+// API CATS and Dogs
 
 
 let storeCatPics = [];
@@ -100,6 +52,77 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+const $name = $('#name')
+const $thumbNail = $('#thumbNail')
+const $instructions = $('#instructions')
+// const $weather = $('#weather')
+// const $input = $('input[type="text"]')
+
+//RapidAPI Tasty API Coffee/Cocktails;
+let drinkData= [];
+
+
+
+$('form').on('submit', handleGetData);
+function handleGetData(event) {
+    event.preventDefault();
+    let userInput= $input.val();
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=" + userInput,
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "9fa51d6725msha4068349b62f730p12c6d2jsnbe63b8ef2e2d",
+            "X-RapidAPI-Host": "tasty.p.rapidapi.com"
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        drinkData = response;
+        render()
+        console.log(response);
+    });
+}
+
+
+function render() {
+    //for of loop that goes through drink data, make the li, append to the array.
+
+    let drinks= $(`<li>${drinkData}</li>`)
+    $name.text(drinkData.name);
+    $thumbNail.img(drinkData.thumbnail_url)
+
+    // $temp.text(weatherData.main.temp);
+    // $feelsLike.text(weatherData.main.feels_like)
+    // $weather.text(weatherData.weather[0].main)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // a function to retrieve data from the API
 // fetch(url, {
@@ -145,18 +168,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-//Coffee/Cocktails; RapidAPI Tasty API
-// const settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup",
-// 	"method": "GET",
-// 	"headers": {
-// 		"X-RapidAPI-Key": "9fa51d6725msha4068349b62f730p12c6d2jsnbe63b8ef2e2d",
-// 		"X-RapidAPI-Host": "tasty.p.rapidapi.com"
-// 	}
-// };
 
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
+
+
+
+// NOt Rapid CATS API
+// const promise = $.ajax(
+//     'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_dyq3XlD3i6ROIfc1C5svJpaWpdLzo5dQyso9rpuUoThjnPfIPgmN5uvA1ylbQ59y'
+	
+// );
+
+// promise.then(
+//   (data) => {
+//    console.log(data);
+//   },
+//   (error) => {
+//    console.log('bad request: ', error);
+//   }
+// );
+
+// const url = "https://api.thecatapi.com/v1/images/search?limit=10";
+
+// const api_key = "live_dyq3XlD3i6ROIfc1C5svJpaWpdLzo5dQyso9rpuUoThjnPfIPgmN5uvA1ylbQ59y";
